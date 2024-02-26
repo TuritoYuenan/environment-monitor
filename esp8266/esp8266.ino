98b823152ae482deee569790372ab0f1ec19374f
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-02-26 19:26:48 +0700

    many updates - rename library file - extract secrets file - change end-of-line to LF - add timer library to make code non-blocking - revert back to dynamic IP

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -7,8 +7,12 @@
  * @ref https://pijaeducation.com/serial-print-and-printf-solved
 */
 
+#include <BlockNot.h>
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
-#include "WetterLib.h"
+
+#include "secrets.h"
+#include "wetter-lib.h"
 
 /// @brief Raw data received from the weather station
 char stationData[35];
@@ -16,46 +20,14 @@ char stationData[35];
 /// @brief Structured weather station data
 WeatherData weatherData;
 
-/// @brief Web server
+/// @brief Server for web dashboard
 ESP8266WebServer server(80);
 
-/// @brief Fixed IP Address to access web server
-IPAddress address(69, 69, 69, 69);
+/// @brief Timer for data getting routine
+BlockNot dataTimer(1000);
 
-/// @brief Default gateway
-IPAddress gateway(69, 69, 69, 1);
-
-/// @brief Subnet mask
-IPAddress subnet(255, 255, 255, 0);
-
-/// @brief Web server HTML template
-String htmlTemplate = R"(<!DOCTYPE html><html lang='en'><head>
-	<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'>
-	<title>Zimmer Wetter</title><link rel='icon' href='data:,'><style>
-		#title {display: grid; align-content: center; text-align: center;} #title > * {margin-block: 0.5rem;}
-		.grid {display: grid; gap: 1rem;} @media (width <= 900px) {.grid {display: flex; flex-direction: column;}}
-		body {max-width: 1280px; margin: auto; padding: 1rem; background: #ED1C2E; font-family: system-ui, sans-serif;}
-		body > * {margin-bottom: 1rem;} header {grid-template-columns: auto 1fr;} * {box-sizing: border-box;}
-		main {grid-template-columns: 1fr 1fr 1fr; grid-template-areas: 'tp ht ps' 'wd wd rn';}
-		h1, h2, p {margin-block: 0.8rem;} h1 {font-size: 3rem;} p {font-size: 3rem; text-align: end;}
-		div, section, footer {padding: 1rem 2rem; border-radius: 1rem; background: white;}
-		img {margin: auto; padding-inline: 1rem; display: block;}
-	</style><script>setInterval(() => {var x = new XMLHttpRequest(); x.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {document.body.innerHTML = this.responseText}
-	}; x.open('GET', '/', true); x.send(); }, 1000);</script>
-</head><body><header class='grid'>
-		<div><img src='https://raw.githubusercontent.com/TuritoYuenan/zimmer-wetter/init/web-server/swinburne.svg' alt='Swinburne University Logo'></div>
-		<div id='title'><h1>Minh Triet's Weather Station</h1><h2>Made with Arduino Uno, ESP8266, SEN0186, and passion🔥</h2></div>
-	</header><main class='grid'>
-		<section style='grid-area: tp'><h2>Temperature</h2><p>{TEMP} &deg;C</p></section>
-		<section style='grid-area: ht'><h2>Humidity</h2><p>{HMDT} %</p></section>
-		<section style='grid-area: ps'><h2>Barometric Pressure</h2><p>{PRSR} Pa</p></section>
-		<section style='grid-area: rn'><h2>Rainfall (1 Hour)</h2><p>{RNFH} mm</p><h2>Rainfall (1 Day)</h2><p>{RNFD} mm</p></section>
-		<section style='grid-area: wd'><h2>Wind Direction</h2><p>{WDRT} degrees</p>
-			<h2>Average Wind Speed</h2><p>{WSAG} m/s</p><h2>Maximum Wind Speed</h2><p>{WSMX} m/s</p></section>
-	</main><footer>&COPY; 2024 Nguyen Ta Minh Triet @
-		<a target='_blank' href='https://turitoyuenan.pages.dev'>turitoyuenan.pages.dev</a></footer>
-</body></html>)";
+/// @brief Timer for data printing routine
+BlockNot logTimer(3000);
 
 /// @brief Tasks to do once at startup
 void setup()
@@ -76,18 +48,42 @@ void setup()
 /// @brief Tasks to routinely do
 void loop()
 {
-	// Get data from weather station. Redo if format is wrong
-	Serial.readBytes(stationData, sizeof(stationData));
-	if (stationData[0] != 'c') { return; }
+	if (dataTimer.TRIGGERED) {
+		// Get data from weather station. Redo if format is wrong
+		Serial.readBytes(stationData, sizeof(stationData));
+		if (stationData[0] != 'c') { return; }
 
-	// Convert and store weather station data
-	weatherData = storeData(stationData);
+		// Convert and store weather station data
+		weatherData = storeData(stationData);
+	}
 
-	// Print out data. Might be removed in favor of web server
-	printData(weatherData);
+	if (logTimer.TRIGGERED) {
+		// Print out data. Might be removed in favor of web server
+		printData(weatherData);
+
+	}
 
 	// Serve webpage if requested
 	server.handleClient();
+}
+
+/// @brief Connect to WiFi
+/// @param ssid WiFi name / SSID
+/// @param password WiFi password
+void connectWiFi(const char* ssid, const char* password)
+{
+	Serial.printf("\nWiFi is %s. Connecting.", ssid);
+	WiFi.begin(ssid, password);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(1000);
+		Serial.print(".");
+	}
+
+	if (WiFi.status() == WL_CONNECTED) {
+		Serial.print("\nConnected! View your weather at: ");
+		Serial.println(WiFi.localIP());
+	}
 }
 
 /// @brief Convert characters to integers
@@ -147,30 +143,6 @@ void printData(WeatherData data)
 	Serial.printf("Rainfall: (1h) %f mm, (1d) %f mm\n", data.rainfallH, data.rainfallD);
 	Serial.printf("Wind Speed: avg %f m/s, max %f m/s\n", data.windSpeedAvg, data.windSpeedMax);
 	Serial.printf("Wind Direction: %d deg\n", data.windDirection);
-}
-
-/// @brief Connect to WiFi
-/// @param ssid WiFi name / SSID
-/// @param password WiFi password
-void connectWiFi(const char* ssid, const char* password)
-{
-	Serial.printf("\nWiFi is %s. Connecting.", ssid);
-
-	if (!WiFi.config(address, gateway, subnet)) {
-		Serial.println("Could not configure web server");
-	}
-
-	WiFi.begin(ssid, password);
-
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(1000);
-		Serial.print(".");
-	}
-
-	if (WiFi.status() == WL_CONNECTED) {
-		Serial.print("\nConnected! View your weather at: ");
-		Serial.println(WiFi.localIP());
-	}
 }
 
 /// @brief Fill weather station data into a HTML file for web server
---
+++ esp8266/secrets.template.h
@@ -0,0 +1,11 @@
+/// @brief Astra database endpoint
+#define DB_ENDPOINT "https://youtube.com/watch?v=dQw4w9WgXcQ"
+
+/// @brief Astra database token
+#define ASTRA_TOKEN "AstraCS:<24 characters string>:<64 characters string>"
+
+/// @brief WiFi name (SSID)
+#define WIFI_NAME "Airport WiFi!! (Totally not Suspicious!!)"
+
+/// @brief WiFi password
+#define WIFI_PASS "joemama"
