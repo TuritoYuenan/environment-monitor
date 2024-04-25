d481e1917f085319f55352121bd4adebeaaf5369
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-04-25 11:00:15 +0700

    Branching process: remove old code

--- 0.5_esp32-c3/secrets.example.h
+++
@@ -1,8 +0,0 @@
-// Blynk Settings
-#define BLYNK_TEMPLATE_ID "Insert your Template ID"
-#define BLYNK_TEMPLATE_NAME "Insert your Template Name"
-#define BLYNK_AUTH_TOKEN "Insert your Blynk token"
-
-// WiFi Settings
-#define WIFI_SSID "Insert your WiFi name"
-#define WIFI_PASS "Insert your WiFi password"
