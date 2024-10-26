55103384e250b14122a3983eeb92fbc4afb28568
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-10-26 21:20:24 +0700

    add broker env to esp32 secret template

--- esp32/secrets.template.h
+++ esp32/secrets.template.h
@@ -8,12 +8,21 @@
 #define MQTT_PORT 1883
 
 /// @brief MQTT Topic
-#define MQTT_TOPIC "#"
+#define MQTT_TOPIC "swinburne/hcmc_a35"
 
 /// @brief WiFi name (SSID)
 #define WIFI_NAME "Airport WiFi!! (Totally not Suspicious!!)"
 
 /// @brief WiFi password
 #define WIFI_PASS "joemama"
+
+/// @brief MQTT Broker client ID
+#define BROKER_CLIENT_ID "ESP32 Weather Station"
+
+/// @brief MQTT Broker username
+#define BROKER_USERNAME "swinuser"
+
+/// @brief MQTT Broker password
+#define BROKER_PASSWORD "swinpass"
 
 #endif // MARK: SECRET_DATA
