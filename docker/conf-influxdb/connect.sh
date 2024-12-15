8ef63af8b8a76604060c3d31e1151fbaf52c51c6
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-12-15 21:17:24 +0700

    add mock station service

--- docker/conf-influxdb/connect.sh
+++ docker/conf-influxdb/connect.sh
@@ -1,11 +1,11 @@
 influx remote create \
 	--name cloud-db \
 	--remote-url https://eu-central-1-1.aws.cloud2.influxdata.com \
-	--remote-api-token MlOhaDEFRVN_fVrMVw_iMcyurl46fFNNB6N-K2Gj8FF3zR1JCyVesTrD9ntH7DYJthiWmzH9bHqbV9O1QVHKdg== \
+	--remote-api-token S_ghct9y6D0eLqIj-cXu3fJ1rnPEd5-9OTLvqMKXBMWMro9IMhFRLoo8SEv_BqJfpGXZO7tzEa0aNFF94W3WNw== \
 	--remote-org-id 5f839e673fe78bb5
 
 influx replication create \
 	--name local-to-cloud \
-	--remote-id 0e0842dce2fb7000 \
+	--remote-id 0e1eaa16b2e9d000 \
 	--local-bucket-id 161cc161c88ad7bc \
 	--remote-bucket b12929acdb8017ca
---
+++ mock-station/main.py
@@ -0,0 +1,63 @@
+import json, random, time
+from paho.mqtt import client, enums
+
+
+# MARK: Constants
+DEBUG_MODE = True
+CLIENT_ID = 'Mock Weather Station'
+BROKER_HOST = 'broker' # name of Mosquitto service in compose.yml
+BROKER_PORT = 1883
+TOPIC = 'swinburne/hcmc_a35'
+
+
+# MARK: Connection callback
+def on_connect(client: client.Client, userdata, flags, reason_code, properties):
+	"""Routine to do when connected to the MQTT broker
+
+	Args:
+		client (Client): MQTT Client
+		userdata (InfluxDBClient): Client to save data
+		flags (unknown): Program flags
+		reason_code (_type_): _description_
+		properties (_type_): _description_
+	"""
+	print('Connected to broker')
+
+
+# MARK: Generate data
+def generate_data(client: client.Client):
+	mock_data: dict[str, int | float] = {
+		'humidity': random.randint(0, 100),
+		'pressure': random.randint(100000, 102000),
+		'temperature': random.uniform(10, 40),
+		'rainfall_day': random.uniform(0, 10),
+		'rainfall_hour': random.uniform(0, 10),
+		'wind_speed_avg': random.uniform(0, 60),
+		'wind_speed_max': random.uniform(0, 60),
+		'wind_direction': random.randint(0, 360)
+	}
+
+	if DEBUG_MODE: print(mock_data)
+	client.publish(TOPIC, json.dumps(mock_data))
+
+
+# MARK: Main procedure
+def main():
+	"""Main procedure
+	"""
+	mqtt_client = client.Client(enums.CallbackAPIVersion.VERSION2, CLIENT_ID)
+
+	print("Setting up callback functionality")
+	mqtt_client.on_connect = on_connect
+
+	print("Connecting to broker")
+	mqtt_client.connect(BROKER_HOST, BROKER_PORT)
+
+	print("Connected! Start generating weather data")
+	while True:
+		generate_data(mqtt_client)
+		time.sleep(1)
+
+
+# MARK: Main execution
+if __name__ == '__main__': main()
